4cb2f282e671bb561dad1d34f8b8d47ac937ce50
Hong Minhee <minhee@dahlia.kr>
2013-05-28 06:27:59 +0900

    Fix file_test on Python 3

--- tests/file_test.py
+++ tests/file_test.py
@@ -1,15 +1,17 @@
+from __future__ import unicode_literals
+
 import io
 
 from sqlalchemy_imageattach.file import ReusableFileProxy
 
 
 def test_reusable_file_proxy():
-    buffer_ = io.BytesIO('abcde' * 100)
+    buffer_ = io.BytesIO(b'abcde' * 100)
     buffer_.seek(20)
     with ReusableFileProxy(buffer_) as proxy:
         assert proxy.tell() == 0
-        assert proxy.read() == 'abcde' * 100
+        assert proxy.read() == b'abcde' * 100
         assert proxy.tell() == 500
     assert buffer_.tell() == 20
-    assert buffer_.read() == 'abcde' * 96
+    assert buffer_.read() == b'abcde' * 96
     buffer_.close()
