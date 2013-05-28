298944876eac72f308de0e14661fb4ec4ae86095
Hong Minhee <minhee@dahlia.kr>
2013-05-28 14:51:17 +0900

    Add S3Store

---
+++ sqlalchemy_imageattach/stores/s3.py
@@ -0,0 +1,221 @@
+""":mod:`sqlalchemy_imageattach.stores.s3` --- AWS S3_ backed image storage
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+`Amazon Web Services <AWS>`_ offers `Simple Storage Service <S3>`_.
+It provides several features we really need:
+
+Accessible via HTTP(S)
+   Comic images should be accessibile from web browser.
+   S3 objects can have their own public URL if they are set to enough ACL.
+
+Reliability
+   Comic images shouldn't be lost.
+
+Redundancy control
+   - Reduced Redundancy: resized images could be reproducible by computing.
+   - Standard: original images cannot be reproducible,
+     so it shouldn't be lost.
+
+Low cost
+   We have no enough money.
+
+In here we depend on :mod:`simples3` to deal with S3 buckets and objects.
+
+.. _S3: http://aws.amazon.com/s3/
+.. _AWS: http://aws.amazon.com/
+
+"""
+import mimetypes
+import re
+
+from simples3.bucket import S3Bucket
+
+from ..store import Store
+
+__all__ = 'S3SandboxStore', 'S3Store'
+
+
+#: (:class:`numbers.Integral`) The default ``max-age`` seconds of
+#: :mailheader:`Cache-Control`.  It's the default value of
+#: :attr:`S3Store.max_age` attribute.
+DEFAULT_MAX_AGE = 60 * 60 * 24 * 365
+
+
+class S3Store(Store):
+    """Image storage backend implementation using S3_.  It implements
+    :class:`~sqlalchemy_imageattach.store.Store` interface.
+
+    :param name: the buckect name, or bucket object provided by :mod:`simples3`
+    :type name: :class:`basestring`, :class:`simples3.bucket.S3Bucket`
+    :type access_key: AWS access key for the bucket.
+                      it can't be applied if ``name`` isn't string
+    :type access_key: :class:`basestring`
+    :type secret_key: AWS secret key for the bucket.
+                      it can't be applied if ``name`` isn't string
+    :type secret_key: :class:`basestring`
+    :param max_age: the ``max-age`` seconds of :mailheader:`Cache-Control`.
+                    default is :const:`DEFAULT_MAX_AGE`
+    :type max_age: :class:`numbers.Integral`
+    :param prefix: the optional key prefix to logically separate stores
+                   with the same bucket.  not used by default
+    :type prefix: :class:`basestring`
+
+    """
+
+    #: (:class:`simples3.bucket.S3Bucket`) The S3 bucket object.
+    bucket = None
+
+    #: (:class:`numbers.Integral`) The ``max-age`` seconds of
+    #: :mailheader:`Cache-Control`.
+    max_age = None
+
+    #: (:class:`basestring`) The optional key prefix to logically separate
+    #: stores with the same bucket.
+    prefix = None
+
+    def __init__(self, name, access_key=None, secret_key=None,
+                 max_age=DEFAULT_MAX_AGE, prefix=''):
+        if isinstance(name, S3Bucket):
+            if access_key or secret_key:
+                raise TypeError('cannot appliable access_key, nor secret_key '
+                                'with simples3.bucket.S3Bucket argument')
+            self.bucket = name
+        else:
+            base_url = 'https://{0}.s3.amazonaws.com'.format(name)
+            self.bucket = S3Bucket(
+                name,
+                access_key=access_key,
+                secret_key=secret_key,
+                base_url=base_url
+            )
+        self.max_age = max_age
+        self.prefix = prefix.strip()
+        if self.prefix.endswith('/'):
+            self.prefix = self.prfix[:-1]
+
+    def get_key(self, object_type, object_id, width, height, mimetype):
+        key = '{0}/{1}/{2}x{3}{4}'.format(
+            object_type, object_id, width, height,
+            mimetypes.guess_extension(mimetype)
+        )
+        if self.prefix:
+            return '{0}/{1}'.format(self.prefix, key)
+        return key
+
+    def get_file(self, *args, **kwargs):
+        key = self.get_key(*args, **kwargs)
+        try:
+            return self.bucket.get(key)
+        except KeyError as e:
+            raise IOError(str(e))
+
+    def get_url(self, *args, **kwargs):
+        key = self.get_key(*args, **kwargs)
+        url = self.bucket.make_url(key)
+        return re.sub(r'\?.*$', '', url)
+
+    def put_file(self, file, object_type, object_id, width, height, mimetype,
+                 reproducible):
+        key = self.get_key(object_type, object_id, width, height, mimetype)
+        rrs = 'REDUCED_REDUNDANCY' if reproducible else 'STANDARD'
+        self.bucket.put(
+            key,
+            file.read(),
+            acl='public-read',
+            mimetype=mimetype,
+            headers={
+                'Cache-Control': 'max-age=' + str(self.max_age),
+                'x-amz-storage-class': rrs
+            }
+        )
+
+    def delete_file(self, *args, **kwargs):
+        key = self.get_key(*args, **kwargs)
+        self.bucket.delete(key)
+
+
+class S3SandboxStore(Store):
+    """It stores images into physically two separated S3 buckets while
+    these look like logically exist in the same store.  It takes two buckets
+    for *read-only* and *overwrite*: ``underlying`` and ``overriding``.
+
+    It's useful for development/testing purpose, because you can use
+    the production store in sandbox.
+
+    :param underlying: the name (or object provided by :mod:`simples3`) of
+                       *underlying* bucket for read-only
+    :type underlying: :class:`basestring`, :class:`simples3.bucket.S3Bucket`
+    :param overriding: the name (or object provided by :mod:`simples3`) of
+                       *overriding* bucket to record overriding modifications
+    :type overriding: :class:`basestring`, :class:`simples3.bucket.S3Bucket`
+    :type access_key: AWS access key for the buckets.
+                      it can't be applied if bucket names are not string
+    :type access_key: :class:`basestring`
+    :type secret_key: AWS secret key for the bucket.
+                      it can't be applied if bucket names are not string
+    :type secret_key: :class:`basestring`
+    :param max_age: the ``max-age`` seconds of :mailheader:`Cache-Control`.
+                    default is :const:`DEFAULT_MAX_AGE`
+    :type max_age: :class:`numbers.Integral`
+    :param overriding_prefix: means the same to :attr:`S3Store.prefix` but
+                              it's only applied for ``overriding``
+    :type overriding_prefix: :class:`basestring`
+    :param underlying_prefix: means the same to :attr:`S3Store.prefix` but
+                              it's only applied for ``underlying``
+    :type underlying_prefix: :class:`basestring`
+
+    """
+
+    #: All keys marked as "deleted" have this mimetype as
+    #: its :mailheader:`Content-Type` header.
+    DELETED_MARK_MIMETYPE = 'application/x-pop-sandbox-deleted'
+
+    #: (:class:`S3Store`) The *underlying* store for read-only.
+    underlying = None
+
+    #: (:class:`S3Store`) The *overriding* store to record overriding
+    #: modification.
+    overriding = None
+
+    def __init__(self, underlying, overriding,
+                 access_key=None, secret_key=None, max_age=DEFAULT_MAX_AGE,
+                 underlying_prefix='', overriding_prefix=''):
+        self.underlying = S3Store(underlying,
+                                  access_key=access_key, secret_key=secret_key,
+                                  max_age=max_age, prefix=underlying_prefix)
+        self.overriding = S3Store(overriding,
+                                  access_key=access_key, secret_key=secret_key,
+                                  max_age=max_age, prefix=overriding_prefix)
+
+    def get_file(self, *args, **kwargs):
+        key = self.overriding.get_key(*args, **kwargs)
+        try:
+            file_ = self.overriding.bucket.get(key)
+        except KeyError:
+            return self.underlying.get_file(*args, **kwargs)
+        if file_.s3_info.get('mimetype') == self.DELETED_MARK_MIMETYPE:
+            raise IOError('deleted')
+        return file_
+
+    def get_url(self, *args, **kwargs):
+        key = self.overriding.get_key(*args, **kwargs)
+        try:
+            self.overriding.bucket.info(key)
+        except KeyError:
+            store = self.underlying
+        else:
+            store = self.overriding
+        return store.get_url(*args, **kwargs)
+
+    def put_file(self, *args, **kwargs):
+        self.overriding.put_file(*args, **kwargs)
+
+    def delete_file(self, *args, **kwargs):
+        key = self.overriding.get_key(*args, **kwargs)
+        self.overriding.bucket.delete(key)
+        self.overriding.bucket.put(
+            key, '',
+            acl='private',
+            mimetype=self.DELETED_MARK_MIMETYPE,
+            headers={ 'x-amz-storage-class': 'REDUCED_REDUNDANCY'}
+        )
---
+++ tests/stores/s3_test.py
@@ -0,0 +1,171 @@
+import functools
+import os.path
+import re
+import time
+
+from pytest import fixture, mark, raises, skip
+
+from sqlalchemy_imageattach.stores.s3 import S3SandboxStore, S3Store
+from ..conftest import sample_images_dir
+from .conftest import TestingImage, utcnow
+
+
+remove_query = functools.partial(re.compile(r'\?.*$').sub, '')
+
+
+@fixture
+def s3_store_getter(request):
+    try:
+        name = request.config.getoption('--s3-name')
+    except ValueError:
+        name = None
+    if name is None:
+        skip('--s3-{name,access-key,secret-key} options (and IMAGEATTACH_TEST'
+             '_S3_{NAME,ACCESS_KEY,SECRET_KEY} envvars) were not provided')
+        return
+    try:
+        access_key = request.config.getoption('--s3-access-key')
+    except ValueError:
+        access_key = None
+    try:
+        secret_key = request.config.getoption('--s3-secret-key')
+    except ValueError:
+        secret_key = None
+    return functools.partial(S3Store, name,
+                             access_key=access_key,
+                             secret_key=secret_key)
+
+
+@fixture
+def s3_sandbox_store_getter(request):
+    try:
+        name = request.config.getoption('--s3-name')
+        sandbox_name = request.config.getoption('--s3-sandbox-name')
+    except ValueError:
+        name = None
+    if name is None:
+        skip('--s3-{name,sandbox-name,access-key,secret-key} options '
+             '(and POPTESTS_S3_{NAME,SANDBOX_NAME,ACCESS_KEY,SECRET_KEY} '
+             'envvars) were not provided')
+        return
+    try:
+        access_key = request.config.getoption('--s3-access-key')
+    except ValueError:
+        access_key = None
+    try:
+        secret_key = request.config.getoption('--s3-secret-key')
+    except ValueError:
+        secret_key = None
+    return functools.partial(S3SandboxStore,
+                             underlying=name,
+                             overriding=sandbox_name,
+                             access_key=access_key,
+                             secret_key=secret_key)
+
+
+@mark.parametrize('prefix', ['', 'prefixtest'])
+def test_s3_store(prefix, s3_store_getter):
+    s3 = s3_store_getter(prefix=prefix)
+    image = TestingImage(thing_id=1234, width=405, height=640,
+                         mimetype='image/jpeg', original=True,
+                         created_at=utcnow())
+    image_path = os.path.join(sample_images_dir, 'iu.jpg')
+    with open(image_path, 'rb') as image_file:
+        expected_data = image_file.read()
+        image_file.seek(0)
+        s3.store(image, image_file)
+    with s3.open(image) as actual:
+        actual_data = actual.read()
+    assert expected_data == actual_data
+    expected_key = s3.get_key('testing', 1234, 405, 640, 'image/jpeg')
+    expected_url = s3.bucket.make_url(expected_key)
+    actual_url = s3.locate(image)
+    assert remove_query(expected_url) == remove_query(actual_url)
+    if prefix:
+        no_prefix = s3_store_getter()
+        with raises(IOError):
+            no_prefix.open(image)
+    s3.delete(image)
+    with raises(IOError):
+        s3.open(image)
+    s3.bucket.delete(expected_key)
+
+
+@mark.parametrize(('underlying_prefix', 'overriding_prefix'), [
+    ('under', 'over'),
+    ('', '')
+])
+@mark.slow
+def test_s3_sandbox_store(underlying_prefix, overriding_prefix,
+                          s3_sandbox_store_getter):
+    s3 = s3_sandbox_store_getter(underlying_prefix=underlying_prefix,
+                                 overriding_prefix=overriding_prefix)
+    under = s3.underlying
+    over = s3.overriding
+    id_offset = int(time.time()) - 1325343600
+    if id_offset % 2:  # id_offset is always even
+        id_offset -= 1
+    if not underlying_prefix:
+        id_offset *= -1
+    # Store a fixture image for underlying store
+    under_id = id_offset + 1
+    under_image = TestingImage(thing_id=under_id, width=405, height=640,
+                               mimetype='image/jpeg', original=True,
+                               created_at=utcnow())
+    image_path = os.path.join(sample_images_dir, 'iu.jpg')
+    with open(image_path, 'rb') as image_file:
+        expected_data = image_file.read()
+        image_file.seek(0)
+        under.store(under_image, image_file)
+    # Underlying images have to be logically shown through sandbox
+    with s3.open(under_image) as actual:
+        actual_data = actual.read()
+    assert expected_data == actual_data
+    expected_key = under.get_key('testing', under_id, 405, 640, 'image/jpeg')
+    expected_url = under.bucket.make_url(expected_key)
+    actual_url = s3.locate(under_image)
+    assert remove_query(expected_url) == remove_query(actual_url)
+    # Store an image to sandbox store
+    over_id = id_offset + 2
+    image = TestingImage(thing_id=over_id, width=405, height=640,
+                         mimetype='image/jpeg', original=True,
+                         created_at=utcnow())
+    image_path = os.path.join(sample_images_dir, 'iu.jpg')
+    with open(image_path, 'rb') as image_file:
+        s3.store(image, image_file)
+    # Image has to be logically stored
+    with s3.open(image) as actual:
+        actual_data = actual.read()
+    assert expected_data == actual_data
+    expected_key = over.get_key('testing', over_id, 405, 640, 'image/jpeg')
+    expected_url = over.bucket.make_url(expected_key)
+    actual_url = s3.locate(image)
+    assert remove_query(expected_url) == remove_query(actual_url)
+    # Image has to be physically stored into the overriding store
+    with over.open(image) as actual:
+        actual_data = actual.read()
+    assert expected_data == actual_data
+    expected_key = over.get_key('testing', over_id, 405, 640, 'image/jpeg')
+    expected_url = over.bucket.make_url(expected_key)
+    actual_url = s3.locate(image)
+    assert remove_query(expected_url) == remove_query(actual_url)
+    # Images must not be physically stored into the underlying store
+    with raises(IOError):
+        under.open(image)
+    # Deletion must not touch underlying
+    s3.delete(under_image)
+    with raises(IOError):
+        s3.open(under_image)
+    with under.open(under_image) as actual:
+        actual_data = actual.read()
+    assert expected_data == actual_data
+    expected_key = over.get_key('testing', under_id, 405, 640, 'image/jpeg')
+    expected_url = over.bucket.make_url(expected_key)
+    actual_url = s3.locate(under_image)
+    assert remove_query(expected_url) == remove_query(actual_url)
+    # Clean up fixtures
+    if underlying_prefix and overriding_prefix:
+        no_prefix = s3_sandbox_store_getter()
+        with raises(IOError):
+            no_prefix.open(image)
+    under.delete(under_image)
