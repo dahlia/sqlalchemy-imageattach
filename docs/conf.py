78ed5be0953527e597f24471b372995a12391070
Hong Minhee <minhee@dahlia.kr>
2013-05-31 06:05:19 +0900

    Mocking wand.api on RTD  [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -25,10 +25,6 @@ from sqlalchemy_imageattach.version import VERSION
 # Mocking C libraries to fake wand.api module which is unavailable
 # on ReadTheDocs builder.
 if os.environ.get('READTHEDOCS', 0):
-    try:
-        import wand.api
-    except ImportError:
-        pass
     class Mock(object):
         def __init__(self, name):
             self.name = name
@@ -36,11 +32,11 @@ if os.environ.get('READTHEDOCS', 0):
             return Mock(self.name + '.' + name)
         def __repr__(self):
             return self.name
-    mockapi = sys.modules['wand._api']
+    mockapi = Mock('wand.api')
+    sys.modules['wand'].api = sys.modules['wand.api'] = mockapi
     mockapi.library = Mock('wand.api.library')
     mockapi.libmagick = Mock('wand.api.libmagick')
     mockapi.libc = Mock('wand.api.libc')
-    sys.modules['wand'].api = sys.modules['wand.api'] = mockapi
 
 
 # -- General configuration -----------------------------------------------------
